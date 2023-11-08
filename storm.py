c2501d9c1df1265d00b6893e7159fd2be2f284e3
Norri2004 <122704111+Norri2004@users.noreply.github.com>
2023-11-08 13:21:22 +0300

    Add files via upload

---
+++ storm.py
@@ -0,0 +1,194 @@
+from telethon.tl.functions.channels import LeaveChannelRequest
+import telethon
+from config import storm
+import config
+from telethon import *
+from telethon.tl import functions, types
+from telethon.tl.functions.channels import GetParticipantRequest, GetFullChannelRequest
+from telethon.errors.rpcerrorlist import UserNotParticipantError
+from telethon.tl.functions.messages import ExportChatInviteRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from time import sleep
+from telethon import events
+import time
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.utils import get_display_name
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import FloodWaitError
+from telethon import TelegramClient, events
+from telethon import functions
+from telethon.errors.rpcerrorlist import (
+    UserAlreadyParticipantError,
+    UserNotMutualContactError,
+    UserPrivacyRestrictedError,
+)
+from telethon.tl.types import InputPeerUser
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl import functions
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+import os
+import datetime
+import requests
+import re
+import help
+
+
+
+
+
+
+@nashr.on(events.NewMessage(outgoing=True, pattern=".مؤقت (.*)"))
+async def spammer(event):
+    reply = await event.get_reply_message()
+    input_str = "".join(event.text.split(maxsplit=1)[1:]).split(" ", 2)
+    sleeptimet = sleeptimem = float(input_str[0])
+    cat = input_str[1:]
+    await event.delete()
+    await spam_function(event, reply, cat, sleeptimem, sleeptimet, DelaySpam=True)
+    
+@nashr.on(events.NewMessage(outgoing=True, pattern=".تكرار (.*)"))
+async def spammer(event):
+    sandy = await event.get_reply_message()
+    cat = ("".join(event.text.split(maxsplit=1)[1:])).split(" ", 1)
+    counter = int(cat[0])
+    if counter > 50:
+        sleeptimet = 0.5
+        sleeptimem = 1
+    else:
+        sleeptimet = 0.1
+        sleeptimem = 0.3
+    await event.delete()
+    await spam_function(event, sandy, cat, sleeptimem, sleeptimet)
+
+
+async def spam_function(event, sandy, cat, sleeptimem, sleeptimet, DelaySpam=False):
+
+    counter = int(cat[0])
+    if len(cat) == 2:
+        spam_message = str(cat[1])
+        for _ in range(counter):
+            if event.reply_to_msg_id:
+                await sandy.reply(spam_message)
+            else:
+                await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+    elif event.reply_to_msg_id and sandy.media:
+        for _ in range(counter):
+            sandy = await event.client.send_file(
+                event.chat_id, sandy, caption=sandy.text
+            )
+            await _catutils.unsavegif(event, sandy)
+            await asyncio.sleep(sleeptimem)
+    elif event.reply_to_msg_id and sandy.text:
+        spam_message = sandy.text
+        for _ in range(counter):
+            await event.client.send_message(event.chat_id, spam_message)
+            await asyncio.sleep(sleeptimet)
+        try:
+            hmm = Get(hmm)
+            await event.client(hmm)
+        except BaseException:
+            pass
+    
+@nashr.on(events.NewMessage(outgoing=True, pattern=".للخاص(?: |$)(.*)"))
+async def gucast(event):
+    nashr = event.pattern_match.group(1)
+    if nashr:
+        msg = nashr
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit(
+            "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
+        )
+        return
+    roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                if chat not in DEVS:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+            except BaseException:
+                er += 1
+    await roz.edit(
+        f"**⌔∮  تم بنجاح الأذاعة الى ** `{done}` **من الدردشات ، خطأ في ارسال الى ** `{er}` **من الدردشات**"
+    )
+    
+@nashr.on(events.NewMessage(outgoing=True, pattern=".للكروبات(?: |$)(.*)"))
+async def gcast(event):
+    nashr = event.pattern_match.group(1)
+    if nashr:
+        msg = nashr
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit(
+            "**⌔∮ يجب الرد على رساله او وسائط او كتابه النص مع الامر**"
+        )
+        return
+    roz = await event.edit("⌔∮ يتم الاذاعة في الخاص انتظر لحضه")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_group:
+            chat = x.id
+            try:
+                if chat not in GCAST_BLACKLIST:
+                    await event.client.send_message(chat, msg)
+                    done += 1
+            except BaseException:
+                er += 1
+    await roz.edit(
+        f"(sec3)"
+    )
+
+@nashr.on(events.NewMessage(outgoing=True, pattern=r"\.ايقاف التكرار"))
+async def update(event):
+    await event.edit("**جاري ايقاف التكرار**")
+    await nashr.disconnect()
+    await nashr.connect()
+    await nashr.send_message("me", "**اكتمل ايقاف التكرار**")
+    
+@nashr.on(events.NewMessage(outgoing=True, pattern=r"\.ايقاف النشر التلقائي"))
+async def update(event):
+    await event.edit(k1)
+    await nashr.disconnect()
+    await nashr.connect()
+    await nashr.send_message("me", "**اكتمل ايقاف النشر التلقائي**")
+
+    
+@nashr.on(events.NewMessage(outgoing=True, pattern=r"\.الاوامر"))
+async def _(event):
+    nashrt = datetime.datetime.now()
+    await event.edit(sec4)
+    
+@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.اعادة تشغيل"))
+async def update(event):
+    await event.edit("• جارِ اعادة تشغيل السورس ..\n• انتضر 1-2 دقيقة  .")
+    await sedthon.disconnect()
+    await sedthon.send_message("me", "`اكتملت اعادة تشغيل السورس !`")
+    
+@nashr.on(events.NewMessage(outgoing=True, pattern=r"\.فحص"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit("جارٍ...")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit(f'''
+**☆ السورس شغال ✅
+☆ VERSION : 1
+☆ PING : `{ms}`
+☆ DATE : `{m9zpi}`
+☆ ID : `{event.sender_id}`
+☆ المطور  : @R_N_G **
+
+-قـم بأرسال `.الاوامر`
+''')
+
+storm.run_until_disconnected()
